99e59b200951cda7f233408b4956e5817e9dc3d1
fibeesoft <36067069+fibeesoft@users.noreply.github.com>
2020-02-10 12:52:29 +0100

    changed resolution to 2:1

--- Fibee Edu/Assets/Scripts/Tasks/MultiplicationTable.cs
+++ Fibee Edu/Assets/Scripts/Tasks/MultiplicationTable.cs
@@ -6,7 +6,7 @@ using TMPro;
 
 public class MultiplicationTable : MonoBehaviour
 {
-    [SerializeField] Text txtExpression, txtMessage;
+    [SerializeField] Text txtExpression;
     [SerializeField] InputField inpAnswer;
     [SerializeField] Slider sliderMaxValue;
     [SerializeField] TextMeshProUGUI txtMaxValue;
@@ -15,23 +15,18 @@ public class MultiplicationTable : MonoBehaviour
     void Start()
     {
         GameManager.instance.SwitchTask(Tasks.MultiplicationTable);
-        ChangeCheckButtonColor(new Color32(0,185,255,255));
-        txtMessage.enabled = false;
         CreateExpression();
-        DisplayMaxValue();
-
     }
-
-
 
     public string Solution()
     {
         return result.ToString();
     }
 
-    void CreateExpression()
+    public void CreateExpression()
     {
         maxValue = (int)sliderMaxValue.value;
+        txtMaxValue.text = "MAX VALUE: " + maxValue;
         num1 = Random.Range(1, maxValue + 1);
         num2 = Random.Range(1, maxValue + 1);
         result = num1 * num2;
@@ -41,22 +36,11 @@ public class MultiplicationTable : MonoBehaviour
         MainUI.instance.SetSolution(Solution());
     }
 
-    public void DisplayMaxValue()
-    {
-        txtMaxValue.text = "MAX VALUE: " + (int)sliderMaxValue.value;
-        CreateExpression();
-    }
-    void ChangeCheckButtonColor(Color c)
-    {
-        txtMessage.color = c;
-    }
-
     public void CheckAnswer()
     {
         if(inpAnswer.text.Trim() == Solution())
         {
             Animations.instance.DisplayResultMessage(true);
-            ChangeCheckButtonColor(Color.green);
             GameManager.instance.AddPoints(1);
             Animations.instance.AnimateCheckButton(true);
             CreateExpression();
@@ -65,18 +49,10 @@ public class MultiplicationTable : MonoBehaviour
         else
         {
             Animations.instance.DisplayResultMessage(false);
-            ChangeCheckButtonColor(Color.red);
             inpAnswer.ActivateInputField();
             Animations.instance.AnimateCheckButton(false);
             inpAnswer.text = "";
         }
-        StartCoroutine(WaitAndChangeButtonColor());
 
-    }
-
-    IEnumerator WaitAndChangeButtonColor()
-    {
-        yield return new WaitForSeconds(1);
-        ChangeCheckButtonColor(new Color32(0, 185, 255, 255));
     }
 }
